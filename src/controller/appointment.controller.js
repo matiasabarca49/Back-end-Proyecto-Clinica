14531ac0cda16af9e5129f3f03789de3c2220cbd
Esteban Espinoza <estebaneugeniok@gmail.com>
2025-02-26 23:47:21 -0300

    Agregado el Paginate en Doctors y Appointments

--- src/controller/appointment.controller.js
+++ src/controller/appointment.controller.js
@@ -4,15 +4,15 @@ const appointmentsManager = new AppointmentsManager();
 export const getAppointments = async (req, res) => {
     const appointmentsGetted = await appointmentsManager.getAppointments();
     appointmentsGetted
-        ? res.status(200).send({ status: "Success", appointments: appointmentsGetted })
-        : res.status(500).send({ status: "ERROR", reason: "Los turnos no se pudieron obtener" });
+        ? res.status(200).send({ status: "Succes", appointments: appointmentsGetted })
+        : res.status(404).send({ status: "ERROR" });
 };
 
 export const getAppointmentById = async (req, res) => {
     const appointmentID = req.params.id;
     const appointmentGetted = await appointmentsManager.getAppointmentById(appointmentID);
     appointmentGetted
-        ? res.status(200).send({ status: "Success", appointment: appointmentGetted })
+        ? res.status(200).send({ status: "Succes", appointment: appointmentGetted })
         : res.status(404).send({ status: "ERROR" });
 };
 
@@ -20,33 +20,51 @@ export const getAppointmentsByFilter = async (req, res) => {
     const filter = req.query;
     const appointmentsGetted = await appointmentsManager.getAppointmentsByFilter(filter);
     appointmentsGetted
-        ? res.status(200).send({ status: "Success", appointments: appointmentsGetted })
+        ? res.status(200).send({ status: "Succes", appointments: appointmentsGetted })
         : res.status(404).send({ status: "ERROR" });
+};
+
+export const getAppointmentsPaginate = async (req, res) => {
+    let defaultQuery, defaultLimit, defaultPage, defaultSort;
+    const { search, query, sort, page, limit } = req.query;
+    console.log(req.query);
+    limit && (defaultLimit = parseInt(limit));
+    page && (defaultPage = parseInt(page));
+    sort && (defaultSort = { date: parseInt(sort) });
+    search.length !== 0
+        ? (defaultQuery = { patientName: search })
+        : query !== "0" && (defaultQuery = { doctorId: query });
+    const appointmentsGetted = await appointmentsManager.getAppointmentsPaginate(defaultQuery, defaultLimit, defaultPage, defaultSort);
+    console.log(appointmentsGetted);
+    appointmentsGetted
+        ? res.status(200).send({ status: "Success", appointments: appointmentsGetted })
+        : res.status(500).send({ status: "ERROR" });
 };
 
 export const createAppointment = async (req, res) => {
     const appointment = req.body;
     const appointmentCreated = await appointmentsManager.createAppointment(appointment);
     appointmentCreated
-        ? res.status(201).send({ status: "Success", appointment: appointmentCreated })
+        ? res.status(201).send({ status: "Succes", appointment: appointmentCreated })
         : res.status(404).send({ status: "ERROR" });
 };
 
 export const deleteAppointment = async (req, res) => {
-    const appointmentID = req.params.id.trim();
+    const appointmentID = req.params.id;
     const appointmentDeleted = await appointmentsManager.deleteAppointment(appointmentID);
     appointmentDeleted
-        ? res.status(200).send({ status: "Success", appointment: appointmentDeleted })
-        : res.status(400).send({ status: "ERROR" });
+        ? res.status(200).send({ status: "Succes", appointment: appointmentDeleted })
+        : res.status(404).send({ status: "ERROR" });
 };
 
 export const updateAppointment = async (req, res) => {
     const appointmentData = req.body;
     const appointmentID = req.params.id;
     console.log(appointmentData);
-
+    
     const appointmentUpdated = await appointmentsManager.updateAppointment(appointmentID, appointmentData);
+    console.log(appointmentUpdated);
     appointmentUpdated
-        ? res.status(201).send({ status: "Success", appointment: appointmentUpdated })
-        : res.status(400).send({ status: "ERROR" });
+        ? res.status(201).send({ status: "Succes", appointment: appointmentUpdated })
+        : res.status(404).send({ status: "ERROR" });
 };
--- src/controller/doctor.controller.js
+++ src/controller/doctor.controller.js
@@ -1,48 +1,70 @@
 import DoctorsManager from "../DAO/mongo/doctors.mongo.js";
 const doctorsManager = new DoctorsManager();
 
-export const getDoctors = async (req, res)=>{
+export const getDoctors = async (req, res) => {
     const doctorsGetted = await doctorsManager.getDoctors();
     doctorsGetted
-    ? res.status(200).send({status: "Succes", doctors : doctorsGetted})
-    : res.status(500).send({status: "ERROR", reason: "Los usuarios no se pudieron obtener"})
-}
+        ? res.status(200).send({ status: "Succes", doctors: doctorsGetted })
+        : res.status(404).send({ status: "ERROR" });
+};
 
-export const getDoctorById = async (req, res)=>{
+export const getDoctorById = async (req, res) => {
     const doctorID = req.params.id;
     const doctorGetted = await doctorsManager.getDoctorById(doctorID);
     doctorGetted
-    ? res.status(200).send({status: "Succes", doctors : doctorGetted}) : res.status(404).send({status: "ERROR"})
-}
+        ? res.status(200).send({ status: "Succes", doctors: doctorGetted })
+        : res.status(404).send({ status: "ERROR" });
+};
 
-export const getDoctorByFilter = async (req, res)=>{
+export const getDoctorByFilter = async (req, res) => {
     const filter = req.query;
     const doctorGetted = await doctorsManager.getDoctorByFilter(filter);
     doctorGetted
-    ? res.status(200).send({status: "Succes", doctors : doctorGetted}) : res.status(404).send({status: "Error"})
-}
+        ? res.status(200).send({ status: "Succes", doctors: doctorGetted })
+        : res.status(404).send({ status: "ERROR" });
+};
 
-export const createDoctor = async (req, res)=>{
-    const doctor = req.body
+export const getDoctorsPaginate = async (req, res) => {
+    let defaultQuery, defaultLimit, defaultPage, defaultSort;
+    const { search, query, sort, page, limit } = req.query;
+    console.log(req.query);
+    limit && (defaultLimit = parseInt(limit));
+    page && (defaultPage = parseInt(page));
+    sort && (defaultSort = { lastName: parseInt(sort) });
+    search.length !== 0
+        ? (defaultQuery = { lastName: search })
+        : query !== "0" && (defaultQuery = { specialty: query });
+    const doctorsGetted = await doctorsManager.getDoctorsPaginate(defaultQuery, defaultLimit, defaultPage, defaultSort);
+    console.log(doctorsGetted);
+    doctorsGetted
+        ? res.status(200).send({ status: "Success", doctors: doctorsGetted })
+        : res.status(500).send({ status: "ERROR" });
+};
+
+export const createDoctor = async (req, res) => {
+    const doctor = req.body;
     const doctorCreated = await doctorsManager.createDoctor(doctor);
     doctorCreated
-    ?res.status(201).send({status: "Succes", doctors : doctorCreated}) : res.status(404).send({status:"ERROR"})
-}
+        ? res.status(201).send({ status: "Succes", doctors: doctorCreated })
+        : res.status(404).send({ status: "ERROR" });
+};
 
-export const deleteDoctor = async (req, res)=>{
-    const doctorID = req.params.id.trim();
+export const deleteDoctor = async (req, res) => {
+    const doctorID = req.params.id;
     const doctorDeleted = await doctorsManager.deleteDoctor(doctorID);
     doctorDeleted
-    ?res.status(200).send({status: "Succes", doctors : doctorDeleted}) : res.status(400).send({status: "ERROR"})
-}
+        ? res.status(200).send({ status: "Succes", doctors: doctorDeleted })
+        : res.status(404).send({ status: "ERROR" });
+};
 
-export const updateDoctor = async (req, res)=>{
+export const updateDoctor = async (req, res) => {
     const doctorData = req.body;
-    const idDoctor=req.params.id;
-    console.log(doctorData)
-
+    const idDoctor = req.params.id;
+    console.log(doctorData);
+    
     const doctorUpdated = await doctorsManager.updateDoctor(idDoctor, doctorData);
+    console.log(doctorUpdated);
     doctorUpdated
-    ? res.status(201).send({status:"Succes", doctors : doctorUpdated}) : res.status(400).send({status:"ERROR"})
-}
-
+        ? res.status(201).send({ status: "Succes", doctors: doctorUpdated })
+        : res.status(404).send({ status: "ERROR" });
+};
