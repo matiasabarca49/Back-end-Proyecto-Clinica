361e4f3127302b83f3fdeaac5058ac8ba67e6ff3
matiasabarca49 <matias49abarca@gmail.com>
2026-08-11 11:53:41 -0300

    fix: Arreglos en middleware de métricas

--- src/middlewares/metrics.middleware.js
+++ src/middlewares/metrics.middleware.js
@@ -1,33 +1,37 @@
+import logger from '../core/logger/logger.js';
 import { httpRequestDuration, httpRequestsTotal, registry } from '../modules/metrics/metrics.js';
 import { performance } from "node:perf_hooks";
 
 export const metricsMiddleware = (req, res, next) => {
   //Evita que el endpoint de metrics se mida
-  console.log(req.path)
   if (req.path === '/api/metrics') {
     return next();
   }
  const start = performance.now();
 
   res.on('finish', async () => {
-    const duration = (performance.now() - start) / 1000;
-
-    const route = req.baseUrl + req.route.path;
-
-    httpRequestsTotal.inc({
-      method: req.method,
-      route,
-      status_code: String(res.statusCode),
-    });
-
-    httpRequestDuration.observe(
-        {
+    try{
+      const duration = (performance.now() - start) / 1000;
+  
+      const route = req.baseUrl + req.route.path;
+  
+      httpRequestsTotal.inc({
         method: req.method,
         route,
         status_code: String(res.statusCode),
-        },
-        duration
-    );
+      });
+  
+      httpRequestDuration.observe(
+          {
+          method: req.method,
+          route,
+          status_code: String(res.statusCode),
+          },
+          duration
+      );
+    }catch(error){
+      logger.error({message: "Error al capturar métrica"})
+    }
   });
 
   next();
